c66fc6203c25bf5dd3801a83f0d97b7f0a6892e9
Ksenia <ksenia.vecherinina@gmail.com>
2014-03-13 11:06:08 +0200

    nachalo

---
+++ table_drag_n_drop/main.js
@@ -0,0 +1,35 @@
+function allowDrop(event) {
+	event.preventDefault();
+}
+
+function drop(event) {
+	event.preventDefault();
+	var what = $('#' + event.dataTransfer.getData("trId"));
+	var where = $(event.target.parentNode);
+	var whatPos = what.position();
+	var wherePos = where.position();
+	if (where.is("tr")) {
+		if (wherePos.top > whatPos.top) {
+			what.insertAfter(where);
+		} else {
+			what.insertBefore(where);
+		}
+		$.each($('tr > td:first-child'), function (index, value) {
+			$(value).text(index + 1);
+		});
+	}
+}
+
+function drag(event) {
+	event.dataTransfer.setData('trId', event.target.id);
+}
+
+$(document).ready(function() {
+	$('tr').each(function() {
+		var trEl = $(this); 
+		trEl.attr('draggable', 'true');
+		trEl.attr('ondragstart', 'drag(event)');
+		trEl.attr('ondragover', 'allowDrop(event)');
+		trEl.attr('ondrop', 'drop(event)');
+	});
+});
